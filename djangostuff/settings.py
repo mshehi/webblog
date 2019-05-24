d5fadb7c7be92081695d99002e00eae7e2fe1797
Megi Shehi <megi.shehi2@gmail.com>
2019-05-24 15:23:00 -0400

    todos

--- djangostuff/settings.py
+++ djangostuff/settings.py
@@ -19,9 +19,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
+# TODO: make file for secret key
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '5lo0t+uf0pjpy)(gkk=@9o_evr*es%s^nkd_zoxr6(udl(#_!l'
 
+# TODO: turn debug off
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig'
 ]
 
 MIDDLEWARE = [
