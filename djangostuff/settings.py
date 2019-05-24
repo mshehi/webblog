caf207b904eb930d68301c3c1d5b02d587fcc0d5
Megi Shehi <megi.shehi2@gmail.com>
2019-05-24 03:05:15 -0400

    todo for mysql

--- djangostuff/settings.py
+++ djangostuff/settings.py
@@ -73,10 +73,11 @@ WSGI_APPLICATION = 'webblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# TODO: use config file to input necessary mysql info
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.path.join(BASE_DIR, 'db.mysql'),
     }
 }
 
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
